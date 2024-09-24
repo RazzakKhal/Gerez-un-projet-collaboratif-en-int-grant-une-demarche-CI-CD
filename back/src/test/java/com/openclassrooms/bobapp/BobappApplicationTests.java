e683e36af5cdffd28f8d6f4ce40819b54cbfdb1f
razzak <khalfallah.razzak@gmail.com>
2024-09-24 23:10:56 +0200

    test echoue

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -8,6 +8,13 @@ class BobappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
+
+	@Test
+    void thisTestWillFail() {
+        // Ce test échoue intentionnellement
+        fail("Ce test est conçu pour échouer.");
+    }
 
 }
